ce1a50994c1b0fc07a6ba60484c4101c597cc9cb
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-04-30 21:03:15 +0200

    avancement des histogrammes entre les variables

--- VerifyData.py
+++ VerifyData.py
@@ -1,8 +1,21 @@
-from import_data import *
+#Import des différentes librairie que l'on à besoin
 import pandas as pd
 import numpy as np
+import numpy as np #linear algebra
+import seaborn as sns #data visualization
+import matplotlib.pyplot as plt #data visualization
+#import sklearn.preprocessing as skp #machine learning (preprocessing)
+#import sklearn.cluster as skc #machine learning (clustering)
 
-Df_dataX,Df_dataNewX,Df_dataY,Df_dataMerge = setup()
+
+#Recupération des données des csv
+Df_dataX = pd.read_csv("Doc\Data_X.csv")
+Df_dataNewX = pd.read_csv("Doc\DataNew_X.csv")
+Df_dataY = pd.read_csv("Doc\Data_Y.csv")
+DfMerge_DataXY = pd.merge(Df_dataX,Df_dataY,on="ID")
+
+# Todo Suppresion de la colonne ID dans le DataFrame DfMerge_DataXY
+
 
 """
 #Fusion les tableaux X et new X
@@ -22,13 +35,15 @@ for ID_data2 in Df_dataY['ID']:
 print("Data Next:",dataNext.shape)
 """
 
+"""
 #2 etape fusionner les data avec le Y grace à l'ID
 merge_data1_Y = pd.merge(Df_dataX,Df_dataY,on="ID")
 merge_data2_Y = pd.merge(Df_dataY,Df_dataY,on="ID")
 print("Merge Df_dataX et Y:",merge_data1_Y.shape)
 print("Merge Df_dataY et Y:",merge_data2_Y.shape)
+"""
 
-#Verification si il y a des valeurs manquantes dans les données
+#Definition des différentes fonctions
 def VerifNullValueByCollumns(data,nameCollumns):
     pourcent=0
     Dimension = data[nameCollumns].shape[0]
@@ -36,14 +51,11 @@ def VerifNullValueByCollumns(data,nameCollumns):
         if value:
             pourcent=+1
     return pourcent
-
 def VerifNullInDataFrameByCollumns(Data):
     listPourcent = []
     for NameCollumns in Data.columns:
         listPourcent.append(VerifNullValueByCollumns(Data,NameCollumns))
     return listPourcent
-
-
 def VerifyNull_RowValue(data,id):
     tuple = [0,0]
     for value in data.iloc[id].isnull():
@@ -52,19 +64,59 @@ def VerifyNull_RowValue(data,id):
         else:
             tuple[1]+=1
     return tuple
-
 def VerifNull_Data(data):
     return data.isnull()
+def NumberOfMisingValue(data):
+    return data.isnull().sum()
+
+#Definition d'une sous partie de mon tableau => limitation de ma machine
+dfTest = DfMerge_DataXY.head(10)
+#Print les info du dataX
+#! print(dfTest.info)
+
+#Print les donnees des 10 premiere lignes
+#!print(dfTest.describe())
+
+#Afficher la sommes des valeurs nulles en fonctions des collones
+#!print(dfTest.isnull().sum()/dfTest.shape[0]*100)
+
+#Afficher les valeurs dupliquers
+#!print(dfTest[dfTest.duplicated(keep=False)])
+
+#Analyse exploratoire des données
+
+    #todo Histogramme des distributions de leur plage de valeurs
+
+#Definition du graphiques
+main = plt.figure(figsize=(12,16))
+for num,collums_name in enumerate(dfTest.describe().columns):
+    plt.subplot(5,2,num+1)
+    sns.distplot(x=dfTest[collums_name])
+    plt.xlabel(collums_name)
+    plt.title('{} Distribution'.format(collums_name))
+    # plt.subplots_adjust(wspace=.2, hspace=.5)
+    plt.tight_layout()
+
+test = "DE_CONSUMPTION"
+#plt.subplot(5,2,4)
+
+#sns.displot(x=dfTest[test])
+plt.xlabel(test)
+plt.title('{} Distribution'.format(test))
+#plt.subplots_adjust(wspace=.2, hspace=1)
+plt.tight_layout()
+#dfTest[test].plot(kind='kde')
+dfTest[test].plot(kind="hist")
+plt.figure
+plt.show()
 
 
-print(VerifNullInDataFrameByCollumns(Df_dataX))
-print(VerifyNull_RowValue(Df_dataX,0))
-print(VerifNull_Data(Df_dataX))
+#todo varaible caractéristique (collonne target) et variable cible(les celle decidé)
+    #hitpogramme voir au dessus
+    #diagramme en boite
+    #graphique de dispersion
 
+#todo correlation des varaibles entre elles 
+#todo correlation entre le colonnes choisie et Target
 
-
-#3 selectioonner les sous ensemble de données
-
-#4 creer les nouvel collone graca au ancienne collone
-
-#5 triée les lignes en fonction des nouvelles collones
+#todo definir clairement ou sont les données
